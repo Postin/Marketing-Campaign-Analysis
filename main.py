efe038c00e36023d66b93674f6f21663ae0e5fd1
Filip Vozarevic <filip.vozarevic@synechron.com>
2021-05-06 13:59:41 +0200

    Uradjena standardizacija, normalizacija, elbow method,kmeans i GMM clustering.

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import seaborn as sns
 from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from scipy.spatial.distance import cdist
 
 df = pd.read_csv('data/marketing_campaign.csv',sep =';')
 #print(df.info())
 
 #print(df.isnull().sum())
-print(df[df['Year_Birth'] == max(df['Year_Birth'])].Income)
+#print(df[df['Year_Birth'] == max(df['Year_Birth'])].Income)
 null_income_df = df[df['Income'].isnull()]
 
 df.loc[(df['Kidhome'] > 0) | (df['Teenhome'] > 0), 'HasChild'] = "Yes"
@@ -64,11 +68,53 @@ df = df.drop(df[df.Income == max(df.Income)].index)
 
 imputer = KNNImputer(n_neighbors=5,metric='nan_euclidean')
 # fit on the dataset
-imputer.fit(df[['Income', 'Age', 'Educational_Years', 'Spending']])
+imputer.fit(df[['Income', 'Spending', 'Seniority']])
 # transform the dataset
-X = imputer.transform(df[['Income', 'Age', 'Educational_Years', 'Spending']])
-Income_impute = pd.DataFrame(X, columns=['Income', 'Age', 'Educational_years', 'Spending'])
+X = imputer.transform(df[['Income', 'Spending', 'Seniority']])
+Income_impute = pd.DataFrame(X, columns=['Income', 'Spending', 'Seniority'])
 df['Income'] = Income_impute['Income'].reset_index(drop=True)
 
-print(df.isnull().sum())
-print(df[df['Income'] == None])
+df = df.dropna()
+
+
+#Standardization
+scaler = StandardScaler()
+scaled_df = df.copy()
+col_names = ['Income','Spending', 'Seniority',]
+features = scaled_df[col_names]
+scaler = scaler.fit(features.values)
+features = scaler.transform(features.values)
+scaled_df[col_names] = features
+print(scaled_df.head())
+
+#Normalization
+transformer = Normalizer()
+col_names = ['Income','Spending', 'Seniority',]
+features = scaled_df[col_names]
+transformer = transformer.fit(features.values)
+features = transformer.transform(features.values)
+scaled_df[col_names] = features
+
+#Elbow method for determining the number of clusters
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k).fit(features)
+    distortions.append(sum(np.min(cdist(features, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / features.shape[0])
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
+
+kmeanModel = KMeans(n_clusters=4).fit(features)
+labels = kmeanModel.predict(features)
+plt.scatter(features[:, 0], features[:, 1], c=labels, cmap='viridis')
+plt.show()
+
+gmm=GaussianMixture(n_components=4, covariance_type='spherical',max_iter=2000, random_state=5).fit(features)
+labels = gmm.predict(features)
+plt.scatter(features[:, 0], features[:, 1], c=labels, cmap='viridis')
+plt.show()
