3a25cf35427edab291197d35be1ac81e9e43a534
Filip Vozarevic <filip.vozarevic@synechron.com>
2021-05-11 10:06:23 +0200

    Uradjen DBSCAN, evaluacija klastera preko silhouette score, vizualizacija Income,Spending i Seniority feature-a nakon normalizacije

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler, Normalizer
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans,DBSCAN
 from sklearn.mixture import GaussianMixture
 from scipy.spatial.distance import cdist
+from sklearn.metrics import silhouette_score
+
 
 df = pd.read_csv('data/marketing_campaign.csv',sep =';')
 #print(df.info())
@@ -95,6 +98,8 @@ transformer = transformer.fit(features.values)
 features = transformer.transform(features.values)
 scaled_df[col_names] = features
 
+
+
 #Elbow method for determining the number of clusters
 distortions = []
 K = range(1,10)
@@ -107,14 +112,47 @@ plt.plot(K, distortions, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Distortion')
 plt.title('The Elbow Method showing the optimal k')
+#plt.show()
+
+kmeanModel = KMeans(n_clusters=4, init="k-means++", max_iter=2000, random_state=42).fit(features)
+labels_kmeans = kmeanModel.predict(features)
+plt.scatter(features[:, 0], features[:, 1], c=labels_kmeans, cmap='viridis')
+#plt.show()
+
+gmm=GaussianMixture(n_components=4, covariance_type='spherical',max_iter=2000, random_state=42).fit(features)
+labels_gmm = gmm.predict(features)
+plt.scatter(features[:, 0], features[:, 1], c=labels_gmm, cmap='viridis')
+#plt.show()
+
+dbscan = DBSCAN(eps=0.5).fit(features)
+plt.scatter(features[:, 0], features[:, 1], c=dbscan.labels_, cmap='viridis')
 plt.show()
 
-kmeanModel = KMeans(n_clusters=4).fit(features)
-labels = kmeanModel.predict(features)
-plt.scatter(features[:, 0], features[:, 1], c=labels, cmap='viridis')
+#evaluacija algoritama klasterovanja
+print('kmeans: {}'.format(silhouette_score(features, kmeanModel.labels_,
+                                           metric='cosine')))
+
+print('GMM: {}'.format(silhouette_score(features, labels_gmm,
+                                           metric='cosine')))
+
+print('DBSCAN: {}'.format(silhouette_score(features, dbscan.labels_,
+                                           metric='cosine')))
+#Interpretacija klastera
+scaled_df['Cluster'] = labels_gmm
+
+#Vizualizacija klastera
+fig = plt.figure(figsize=(6,6))
+ax = Axes3D(fig)
+
+x = scaled_df['Income']
+y = scaled_df['Spending']
+z = scaled_df['Seniority']
+c = scaled_df['Cluster']
+
+ax.set_xlabel("Income")
+ax.set_ylabel("Spending")
+ax.set_zlabel("Seniority")
+
+ax.scatter(xs=x,ys=y,zs=z,c=c)
 plt.show()
 
-gmm=GaussianMixture(n_components=4, covariance_type='spherical',max_iter=2000, random_state=5).fit(features)
-labels = gmm.predict(features)
-plt.scatter(features[:, 0], features[:, 1], c=labels, cmap='viridis')
-plt.show()
