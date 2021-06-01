41dcf4ff5227a3cb5e4b17fa3db51bf66a0226a6
Filip Vozarevic <filip.vozarevic@synechron.com>
2021-06-01 15:59:43 +0200

    Wrapovan keras model u scikit-learn da bi se sistematicnije optimizovali parametri preko gridsearchCV

--- Python Package/tpot_smote_pipeline_f1_50_100.py
+++ Python Package/tpot_smote_pipeline_f1_50_100.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
-from xgboost import XGBClassifier
+from sklearn.pipeline import make_pipeline
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -13,10 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.9439982147763946
+# Average CV score on the training set was: 0.9822109453776605
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=MLPClassifier(alpha=0.0001, learning_rate_init=0.001)),
-    XGBClassifier(learning_rate=1.0, max_depth=10, min_child_weight=19, n_estimators=100, n_jobs=1, subsample=0.55, verbosity=0)
+    PCA(iterated_power=9, svd_solver="randomized"),
+    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.15000000000000002, min_samples_leaf=2, min_samples_split=4, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
